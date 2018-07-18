e91cef642fa872cf68ebe7077c825bb36e784265
ziltoidomni <36250073+ziltoidomni@users.noreply.github.com>
2018-07-17 22:10:26 -0700

    Update sandbox.java

--- src/neural_net/sandbox.java
+++ src/neural_net/sandbox.java
@@ -5,29 +5,16 @@ import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-
 import javax.imageio.ImageIO;
-
 import linearAlgebra.Matrix;
 import linearAlgebra.Vector;
 
 @SuppressWarnings("unused")
 public class sandbox {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		BufferedImage image = ImageIO.read(new File("C:\\Users\\Clayton\\Desktop\\mnist_data\\mnist_jpgfiles\\train\\mnist_2_1670.jpg"));
-		Raster x = image.getData();
-		
-//		System.out.println(trainingImages[0]);
-		
-		for(int i = 0; i < x.getWidth(); i++) {
-			System.out.print("\n");
-			for(int j = 0; j < x.getHeight(); j++) {
-				System.out.print((x.getSampleDouble(i, j, 0) / 255) + " ");
-			}
-		}
-
+		//this is just a test class for the NN
 
 	}
 	
